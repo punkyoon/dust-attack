ef7e0ba8dca9703bf4eef2407569c4ece02d6d93
punkyoon <cryptography@konkuk.ac.kr>
2017-05-14 16:47:11 +0000

    get message from channel

--- bot.py
+++ bot.py
@@ -1,26 +1,29 @@
-import datetime
 import pytz
+import asyncio
+import datetime
+import websockets
 
 from bs4 import BeautifulSoup as bs
 from slacker import Slacker
 
 from conf import *
 
-timezone = pytz.timezone('Asia/Seoul')
+tz = pytz.timezone('Asia/Seoul')
 slack = Slacker(TOKEN)
+
+response = slack.rtm.start()
+sock_endpoint = response.body['url']
 
 def post_to_channel(idx, msg):
     slack.chat.post_message(CHANNELS[idx], msg, as_user=True)
 
-def handler(event, context):
-    ch = event['channel']
-    msg = event['message']
+async def execute_bot():
+    ws = await websockets.connect(sock_endpoint)
+    while True:
+        msg = await ws.recv()
+        print(msg)
 
-    slack.chat.post_message(ch, msg)
-
-if __name__ == '__main__':
-    event = {};
-    event['channel'] = '#general'
-    event['message'] = 'test1'
-
-    handler(event, None)
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+asyncio.get_event_loop().run_until_complete(execute_bot())
+asyncio.get_event_loop().run_forever()
