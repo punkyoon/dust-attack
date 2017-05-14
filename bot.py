b0ae3fe976258559951a737008bcb5cd21287375
punkyoon <cryptography@konkuk.ac.kr>
2017-05-14 18:56:39 +0000

    new cmd...

--- bot.py
+++ bot.py
@@ -19,11 +19,20 @@ def post_to_channel(idx, msg):
     slack.chat.post_message(CHANNELS[idx], msg, as_user=True)
 
 def extract_message(msg):
-    check_cmd = msg.split(' ', 1)
-    if CMD_PREFIX != check_cmd[0]:
+    cmd = msg.split(' ')
+    if CMD_PREFIX != cmd[0]:
         return 'not command'
 
-    post_to_channel(0, 'show me the money')
+    if 1 < len(cmd):
+        if cmd[1] == 'help':
+            post_to_channel(0, 'Can I help you?')
+        elif cmd[1] == 'test':
+            post_to_channel(0, 'Show me the money')
+        else:
+            post_to_channel(0, '????')
+    else:
+        post_to_channel(0, '@dust-attack help')
+
 
 # Get message from slack channel
 async def execute_bot():
@@ -32,8 +41,11 @@ async def execute_bot():
         msg = await ws.recv()
         ext_msg = json.loads(msg)
 
-        if ext_msg['type'] == 'message':
-            extract_message(ext_msg['text'])
+        try:
+            if ext_msg['type'] == 'message':
+                extract_message(ext_msg['text'])
+        except:
+            pass
 
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
