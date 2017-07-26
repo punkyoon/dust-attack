d8715dd53cd5e9c6c3a43c2192fc062f807b2382
punkyoon <cryptography@konkuk.ac.kr>
2017-07-26 16:24:58 +0000

    developed getting aqi status

--- dust.py
+++ dust.py
@@ -1,7 +1,8 @@
 import requests
 
 class Dust():
-    google_url = "https://maps.googleapis.com/maps/api/geocode/json"
+    google_url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    wapi_url = 'https://api.waqi.info/feed/geo:'
 
     def __init__(self, api_key):
         self.api_key = api_key
@@ -10,17 +11,34 @@ class Dust():
         response = requests.get(self.google_url, params={'address': location}).json()
         if response['status'] == 'OK':
             location_code = response['results'][0]['geometry']['location']
-            print(location_code)
             return location_code
         else:
             return None
 
-    def getDust(self, location):
-        pass
+    def getDust(self, location_code):
+        formats = (location_code['lat'], location_code['lng'])
+        url = 'https://api.waqi.info/feed/geo:%s;%s/' % formats
+        response = requests.get(url, params={'token': self.api_key}).json()
+        dust = response['data']['aqi']
+
+        msg = None
+
+        if dust > 300:
+            msg = '위험: 환자군 및 민감군에게 응급 조치가 발생되거나, 일반인에게 유해한 영향이 유발될 수 있는 수준'
+        elif dust > 200:
+            msg = '매우 나쁨: 환자군 및 민감군에게 급성 노출시 심각한 영향 유발, 일반인도 약한 영향이 유발될 수 있는 수준'
+        elif dust > 150:
+            msg = '나쁨: 환자군 및 민감군에게 유해한 영향 유발, 일반인도 건강상 불쾌감을 느낄 수 있는 수준'
+        elif dust > 100:
+            msg = '민감군 영향: 환자군 및 민감군에게 유해한 영향이 유발될 수 있는 수준'
+        elif dust > 50:
+            msg = '보통: 환자군에게 만성 노출시 경미한 영향이 유발될 수 있는 수준'
+        else:
+            msg = '좋음: 대기오염 관련 질환자군에서도 영향이 유발되지 않을 수준'
+        
+        return msg
 
 if __name__ == '__main__':
-    dust = Dust('test')
-    #dust.getLocation('서울')
-    #dust.getLocation('광주')
-    #dust.getLocation('경기')
-    dust.getLocation('서울 광진구')
+    dust = Dust('demo')
+    location = dust.getLocation('서울 광진구')
+    print(dust.getDust(location))
